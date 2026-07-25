d01c8465ee1ecc8bb65cfd9aae4a992c2817ce88
asrawazir <waziralamasra@gmail.com>
2026-07-25 18:51:23 +0500

    Implement initial binaryToDecimal function structure

--- funtions/BinaryToDecimal.cpp
+++ funtions/BinaryToDecimal.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int binaryToDecimal(int bin){
+    if(bin == 0) return 0;
+
+    
+}
+
 int main(){
     cout << "---Binary To Decimal---" << endl;
     return 0;
