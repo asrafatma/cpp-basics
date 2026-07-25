75df174d007c075d3fe9a5c0d6cacb75dac76729
asrawazir <waziralamasra@gmail.com>
2026-07-25 18:59:33 +0500

    Fix binaryToDecimal function to handle long long input and correct conversion logic

--- funtions/BinaryToDecimal.cpp
+++ funtions/BinaryToDecimal.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int binaryToDecimal(int bin){
-    if (dec == 0) return 0;
+int binaryToDecimal(long long bin) {
+    if (bin == 0) return 0;
 
-    long long ans = 0;
-    long long power = 1;
+    int ans = 0;
+    int power = 1;
 
-    while (dec > 0) {
-        int rem = dec % 2;
-        dec = dec / 2;
+    while (bin > 0) {
+        int rem = bin % 10; 
+        bin = bin / 10;
 
         ans += (rem * power);
-        power *= 10;
+        power *= 2; 
     }
 
     return ans;
 }
 
-int main(){
-    int dec;
-    cout << "Enter the decimal number: ";
-    cin >> dec;
+int main() {
+    long long bin;
+    cout << "Enter the binary number: ";
+    cin >> bin;
 
-    long long bin = decimalToBinary(dec);
+    int dec = binaryToDecimal(bin);
 
-    cout << "The binary equivalent of " << dec << " is " << bin << "." << endl;
+    cout << "The decimal equivalent of " << bin << " is " << dec << "." << endl;
     return 0;
 }
