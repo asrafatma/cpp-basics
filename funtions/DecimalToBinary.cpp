d0adc8920949341e9d1471bb329a650ea08303f2
asrawazir <waziralamasra@gmail.com>
2026-07-25 18:33:29 +0500

    Implement initial logic for decimal to binary conversion in DecimalToBinary.cpp

--- funtions/DecimalToBinary.cpp
+++ funtions/DecimalToBinary.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 int decimalToBinary(int dec){
-    
+    int ans = 0
+    int pow = 1;
+
+    while(dec > 0){
+        
+    }
 }
 int main(){
     cout << "---Decimal To Binary---" << endl;
