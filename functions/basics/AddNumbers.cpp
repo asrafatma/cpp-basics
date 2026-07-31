d588fe57c2ca104d95951f42b77ba93b9283afd9
asrawazir <waziralamasra@gmail.com>
2026-07-31 22:14:14 +0500

    Cast user input to int for num1 and num2 in AddNumbers.cpp

--- functions/basics/AddNumbers.cpp
+++ functions/basics/AddNumbers.cpp
@@ -8,6 +8,9 @@ int add(int a, int b){
 int main(){
     int num1, num2;
     cout << "Enter the first number: ";
-    cin >> num1;
+    cin >> (int)num1;
+
+    cout << "Enter the second number: ";
+    cin >> (int)num2;
     return 0;
 }
