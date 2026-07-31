5b7f7b9f21c7607c8c484df77797fd4952854f82
asrawazir <waziralamasra@gmail.com>
2026-07-31 22:03:25 +0500

    Add simpleFunction implementation and call in main

--- functions/basics/SimpleFunction.cpp
+++ functions/basics/SimpleFunction.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+void simpleFunction() {
+    cout << "Hello! This is a simple function." << endl;
+}
+
 int main(){
+    simpleFunction();
     return 0;
 }
