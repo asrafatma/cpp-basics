429628ad8d7935f3d1a2b16db7ae952fbd8bdc46
asrawazir <waziralamasra@gmail.com>
2026-07-31 22:15:20 +0500

    Remove unnecessary type casting for user input in AddNumbers.cpp

--- functions/basics/AddNumbers.cpp
+++ functions/basics/AddNumbers.cpp
@@ -8,10 +8,10 @@ int add(int a, int b){
 int main(){
     int num1, num2;
     cout << "Enter the first number: ";
-    cin >> (int)num1;
+    cin >> num1;
 
     cout << "Enter the second number: ";
-    cin >> (int)num2;
+    cin >> num2;
 
     int sum = add(num1, num2);
     cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << endl;
