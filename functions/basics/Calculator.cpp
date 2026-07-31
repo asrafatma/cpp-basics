7e3c0b7c63fc71d1e367d194a04ad4462e35e143
asrawazir <waziralamasra@gmail.com>
2026-07-31 22:28:57 +0500

    Add multiply function to Calculator.cpp for basic multiplication

--- functions/basics/Calculator.cpp
+++ functions/basics/Calculator.cpp
@@ -8,6 +8,10 @@ double add(double a, double b) {
 double subtract(double a, double b) { 
     return a - b; 
 }
+
+double multiply(double a, double b) { 
+    return a * b; 
+}
 int main(){
     return 0;
 }
