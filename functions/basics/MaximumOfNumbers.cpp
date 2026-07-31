46c5dd263c3d0fb5f96ba4f8a5b23a87ea4872ad
asrawazir <waziralamasra@gmail.com>
2026-07-31 22:22:33 +0500

    Implement getMaxOfThree function to return the maximum of three integers in MaximumOfNumbers.cpp

--- functions/basics/MaximumOfNumbers.cpp
+++ functions/basics/MaximumOfNumbers.cpp
@@ -5,6 +5,16 @@ int getMaxOfTwo(int a, int b) {
     return (a > b) ? a : b;
 }
 
+int getMaxOfThree(int a, int b, int c) {
+    if (a >= b && a >= c) {
+        return a;
+    } else if (b >= a && b >= c) {
+        return b;
+    } else {
+        return c;
+    }
+}
+
 int main(){
     return 0;
 }
