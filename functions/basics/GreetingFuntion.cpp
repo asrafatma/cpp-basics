6d8ef6a53814a3f9e066342fd8952bf9349c6ac9
asrawazir <waziralamasra@gmail.com>
2026-07-31 22:09:02 +0500

    Add greetUser function to display a welcome message in GreetingFunction.cpp

--- functions/basics/GreetingFuntion.cpp
+++ functions/basics/GreetingFuntion.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+void greetUser(std::string name) {
+    cout << "Hello, " << name << "! Welcome to C++ programming." << endl;
+}
+
 int main(){
     return 0;
 }
