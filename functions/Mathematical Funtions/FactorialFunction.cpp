8fad873b7d16903286964cbf9d790c03f411f352
asrafatma <waziralamasra@gmail.com>
2026-08-02 23:05:33 +0500

    Add calculateFactorial function implementation in FactorialFunction.cpp

--- functions/Mathematical Funtions/FactorialFunction.cpp
+++ functions/Mathematical Funtions/FactorialFunction.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+long calculateFactorial(int n) {
+    unsigned long long fact = 1;
+    for (int i = 1; i <= n; ++i) {
+        fact *= i;
+    }
+    return fact;
+} 
+
 int main(){
     return 0;
 }
