9f0bed97ac269f1665f515849958f621ce9c1cce
Asra Fatma <waziralamasra@gmail.com>
2026-08-18 17:48:54 +0000

    Add comments in SumOfTwoNumbers.cpp for improved readability

--- basics/SumOfTwoNumbers.cpp
+++ basics/SumOfTwoNumbers.cpp
@@ -5,15 +5,19 @@ int main()
 {
     cout << "---SUM OF TWO NUMBERS---" << endl;
 
+    //User input
     double num1;
-    cout << "Enter first number: ";
+    cout << "Enter first number: "; 
     cin >> num1;
 
     double num2;
-    cout << "Enter second number: ";
+    cout << "Enter second number: "; 
     cin >> num2;
 
+    //Calculating sum
     double sum = num1 + num2;
+    
+    //Displaying result
     cout << "Sum: " << sum << endl;
 
     return 0;
