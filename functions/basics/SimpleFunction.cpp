836ed7bc4b16b09cf9afa998821904674441ee4c
Asra Fatma <waziralamasra@gmail.com>
2026-08-18 17:53:56 +0000

    Add comments for function definition and main function in SimpleFunction.cpp

--- functions/basics/SimpleFunction.cpp
+++ functions/basics/SimpleFunction.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 
+//Function Definition
 void simpleFunction() {
     cout << "Hello! This is a simple function." << endl;
 }
 
+//Main function
 int main(){
-    simpleFunction();
+    simpleFunction(); // call to defined function
     return 0;
 }
