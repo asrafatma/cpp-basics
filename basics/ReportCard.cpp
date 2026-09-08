e23078d918d8bca07bfa3346a79d5c9bd0eab966
asrafatma <waziralamasra@gmail.com>
2026-09-08 09:50:25 +0500

    Add ReportCard.cpp to display student ID, score, and grade

---
+++ basics/ReportCard.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int studentID = 123;
+    float score = 78.67;
+    char grade = 'B';
+
+    cout << "Student ID: " << studentID << endl;
+    cout << "Score: " << score << endl;
+    cout << "Grade: " << grade << endl;
+
+    return 0;
+}
