eb0a352a7215a8513e0062dd305a338d771ba1de
asrafatma <waziralamasra@gmail.com>
2026-09-08 09:22:22 +0500

    Add DataTypes.cpp with basic data type declarations and main function

---
+++ basics/DataTypes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myNum = 5;  //4 bytes
+    double myFloatNum = 5.99;  //8 bytes
+    char myLetter = 'D';  //1 byte
+    string myText = "Hello";   // 2 bytes per character
+    bool myBoolean = true;  //1 byte
+    return 0;
+}
