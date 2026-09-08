f2705d4765c8348186f459eca65325e94873482c
asrafatma <waziralamasra@gmail.com>
2026-09-08 10:25:04 +0500

    Add BoolAplha.cpp to demonstrate boolean output with boolalpha

---
+++ basics/BoolAplha.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+
+    cout << boolalpha; // enable printing "true"/"false"
+
+    cout << isCodingFun << "\n";   // Outputs true
+    cout << isFishTasty << "\n"; 
+    return 0;
+}
