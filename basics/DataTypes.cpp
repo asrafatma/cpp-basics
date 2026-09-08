50712bf7b93459b23217a0ceb0624a9dac0efa28
asrafatma <waziralamasra@gmail.com>
2026-09-08 09:22:48 +0500

    Refactor output statements in DataTypes.cpp for clarity and organization

--- basics/DataTypes.cpp
+++ basics/DataTypes.cpp
@@ -7,5 +7,12 @@ int main() {
     char myLetter = 'D';  //1 byte
     string myText = "Hello";   // 2 bytes per character
     bool myBoolean = true;  //1 byte
+
+    cout << myNum << endl;
+    cout << myFloatNum << endl;
+    cout << myLetter << endl;
+    cout << myText << endl;
+    cout << myBoolean << endl;
+
     return 0;
 }
