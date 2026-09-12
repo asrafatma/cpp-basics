7dc4c12961528c66579faf780adc89c282d1ddad
asrafatma <waziralamasra@gmail.com>
2026-09-12 22:12:47 +0500

    Add comments to clarify input age and eligibility determination in VoteEligibilityChecker.cpp

--- basics/VoteEligibilityChecker.cpp
+++ basics/VoteEligibilityChecker.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main()
 {
+    // Input age
     int age;
     cout << "Enter your age: ";
     cin >> age;
 
+    // Determine eligibility
     if (age >= 18)
     {
         cout << "You are eligible to vote." << endl;
