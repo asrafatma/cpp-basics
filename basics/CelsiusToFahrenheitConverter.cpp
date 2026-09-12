9e19ccf00096b2d944d9b89a63f8547763f82353
asrafatma <waziralamasra@gmail.com>
2026-09-12 22:11:16 +0500

    Add comments to clarify input and conversion process in CelsiusToFahrenheitConverter.cpp

--- basics/CelsiusToFahrenheitConverter.cpp
+++ basics/CelsiusToFahrenheitConverter.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main()
 {
+    //input temperature in Celsius
     double celsius;
     cout << "Enter temperature in Celsius: ";
     cin >> celsius;
 
+    //conversion formula from Celsius to Fahrenheit
     double fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
     cout << celsius << " degrees Celsius is equal to " << fahrenheit << " degrees Fahrenheit." << endl;
 
