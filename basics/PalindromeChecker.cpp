85f7b0df1cecbc5472dca821896c88bea8f578ad
asrafatma <waziralamasra@gmail.com>
2026-09-13 22:50:21 +0500

    Add comments to clarify input, palindrome check, and output in PalindromeChecker.cpp

--- basics/PalindromeChecker.cpp
+++ basics/PalindromeChecker.cpp
@@ -5,18 +5,22 @@ int main()
 {
     cout << "--- Palindrome Checker ---" << endl;
 
+    // Input a number
     int number;
     cout << "Enter a number: ";
     cin >> number;
 
+    // Check if the number is a palindrome
     int originalNumber = number;
     int reversedNumber = 0;
 
+    // Reverse the number
     while (number > 0) {
         reversedNumber = reversedNumber * 10 + number % 10;
         number /= 10;
     }
 
+    // Output the result
     if (originalNumber == reversedNumber) {
         cout << originalNumber << " is a palindrome." << endl;
     } else {
