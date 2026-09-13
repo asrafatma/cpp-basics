86e334710c55c2ef93f149e895d38860e98bf26d
asrafatma <waziralamasra@gmail.com>
2026-09-13 22:49:49 +0500

    Add comments to clarify input and validation in PrimeNumberFinder.cpp

--- basics/PrimeNumberFinder.cpp
+++ basics/PrimeNumberFinder.cpp
@@ -5,19 +5,23 @@ int main()
 {
     cout << "--- Prime Number Finder ---" << endl;
 
+    // Input starting and ending numbers
     cout << "Enter the starting number: ";
     int start;
     cin >> start;
 
+    // Input ending number
     cout << "Enter the ending number: ";
     int end;
     cin >> end;
 
+    // Check if starting number is less than or equal to ending number
     if (start > end) {
         cout << "Starting number should be less than or equal to ending number." << endl;
         return 1;
     }
 
+    // Find and print prime numbers in the given range
     for(int num = start; num <= end; num++) {
         bool isPrime = true;
 
